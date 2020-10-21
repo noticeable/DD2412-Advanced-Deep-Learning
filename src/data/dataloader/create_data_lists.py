bc2c6c5c540f64d2697aa61a7568cd3f0456a1c1
Amrita Panesar <panesar@kth.se>
2020-10-22 00:55:10 +0200

    Extract main class for each image VOC2007 Detection dataset + refactoring of ddatasets class

--- src/data/dataloader/create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(voc07_path=r'E:\VOC\2007merge',
-                      voc12_path=r'E:\VOC\2012\VOCdevkit\VOC2012',
-                      output_folder=r'E:\VOC\loader')
