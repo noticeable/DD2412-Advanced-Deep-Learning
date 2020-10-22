94d0c4413b57ffb1d43c7dbf537fb9baf38f670b
Amrita Panesar <panesar@kth.se>
2020-10-22 13:01:03 +0200

    Initial transform of bounding boxes and loading boxes in dataloader

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -8,23 +8,33 @@ from torch.utils.data import Dataset
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
+import numpy as np
 
-from data.utils import transform
+from data.utils import clip_box
 from data.transform_voc import ToLabel, Relabel
 
 EXTENSIONS = ['.jpg', '.png']
 
 input_transform = transforms.Compose([
-        transforms.CenterCrop(256),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[.485, .456, .406], std=[.229, .224, .225]),
+    transforms.CenterCrop(256),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[.485, .456, .406], std=[.229, .224, .225]),
     ])
 
 target_transform = transforms.Compose([
     transforms.CenterCrop(256),
     ToLabel(),
     Relabel(255, 21),
-])
+]) # TODO come back to
+
+def bb_transform(bbox, im_width, im_height):
+    # bbox is (x,y,w,h)
+    bbox = np.array(bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]) # (x1,y1,x2,y2)
+    # center crop on image so need to get same border here
+    crop_x = int((im_width - 256) / 2)
+    crop_y = int((im_height - 256) / 2)
+    bbox = clip_box(bbox, np.array([crop_x, crop_x+256, crop_y, crop_y+256])) # crop
+    return torch.tensor([bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]-bbox[1]])
 
 
 def load_image(file):
@@ -51,6 +61,7 @@ class VOC2012(Dataset):
         self.input_transform = input_transform
         self.target_transform = target_transform
 
+
     def __getitem__(self, index):
         filename = self.filenames[index]
 
@@ -76,15 +87,16 @@ class VOC2007(Dataset):
     Taken and modified from https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Object-Detection
     """
 
-    def __init__(self, data_folder, split, return_bb=False, input_transform=None):
+    def __init__(self, data_folder, split, return_bb=False, input_transform=None, box_transform=None):
         """
         :param data_folder: folder where data files are stored
         :param split: split, one of 'TRAIN' or 'TEST'
-        :param return_bb: if True, returns bounding boxes instead of labels
+        :param return_bb: if True, returns bounding boxes as well
         """
         self.split = split.upper()
         self.return_bb = return_bb
         self.input_transform = input_transform
+        self.box_transform = box_transform
 
         assert self.split in {'TRAIN', 'VAL', 'TEST'}
 
@@ -93,59 +105,33 @@ class VOC2007(Dataset):
         # Read data files
         with open(os.path.join(data_folder, self.split + '_images.json'), 'r') as j:
             self.images = json.load(j)
-        with open(os.path.join(data_folder, self.split + '_objects.json'), 'r') as j:
-            self.objects = json.load(j)
+
         # file with label corresponding to largest object
-        self.labels = pd.read_csv(os.path.join(data_folder, self.split + '_largest_item_labels.csv'))['labels'].tolist()
+        labels_boxes = pd.read_csv(os.path.join(data_folder, self.split + '_largest_item_labels.csv'))
+        self.labels = labels_boxes['labels'].tolist()
+        self.boxes = labels_boxes['boxes'].tolist()
 
         assert len(self.images) == len(self.objects)
 
     def __getitem__(self, i):
         # Read image
-        if self.return_bb:
-            image = Image.open(self.images[i], mode='r')
-            image = image.convert('RGB')
+        image = load_image(self.images[i]).convert('RGB')
+        im_width, im_height = im.size
+        if self.input_transform is not None:
+            image = self.input_transform(image)
+        label = self.labels[i]
 
-            # Read objects in this image (bounding boxes, labels)
-            objects = self.objects[i]
-            boxes = torch.FloatTensor(objects['boxes'])  # (n_objects, 4)
-            labels = torch.LongTensor(objects['labels'])  # (n_objects)
+        if self.return_bb: # returns bounding box and label
+            box = self.boxes[i]
+            if self.box_transform is not None:
+                box = self.box_transform(box, im_width, im_height)
+            return image, label, box
 
-            # Apply transformations
-            image, boxes, labels = transform(image, boxes, labels, split=self.split)
-            return image, boxes
-        else:
-            image = load_image(self.images[i]).convert('RGB')
-            if self.input_transform is not None:
-                image = self.input_transform(image)
-            label = self.labels[i]
-            return image, label
+        return image, label
 
 
     def __len__(self):
         return len(self.images)
-
-    # def collate_fn(self, batch):
-    # TODO come back to
-    #     """
-    #     Since each image may have a different number of objects, we need a collate function (to be passed to the DataLoader).
-    #
-    #     :param batch: an iterable of N sets from __getitem__()
-    #     :return: a tensor of images, lists of varying-size tensors of labels.
-    #             NOTE: Labels are bounding boxes if self.return_bb is True, otherwise are class labels
-    #     """
-    #     if self.return_bb:
-    #         images = list()
-    #         labels = list() # contains Detection labels (class labels) if self.return_bb is false, otherwise returns bounding boxes
-    #
-    #         for b in batch:
-    #             images.append(b[0])
-    #             labels.append(b[1])
-    #
-    #         images = torch.stack(images, dim=0)
-    #         return images, labels
-    #     else:
-    #         return batch
 
 
 if __name__ == '__main__':
@@ -159,7 +145,7 @@ if __name__ == '__main__':
     datadir = args.data_dir
     dataset = args.dataset
     if dataset == 'VOC2012':
-        # VOC2012 (Segmentation) dataloader
+        #####  VOC2012 (Segmentation) dataloader #####
         loader = DataLoader(VOC2012(datadir, input_transform, target_transform),
                             num_workers=1, batch_size=args.batch_size, shuffle=True)
 
@@ -171,8 +157,9 @@ if __name__ == '__main__':
                 targets = Variable(labels)
                 # outputs = model(inputs)
     else:
+        ##### VOC2007 (Detection) dataloader #####
         print('Loading VOC2007 dataset...')
-        # VOC2007 (Detection) dataloader
+        ### Get transformed image, label ###
         train_dataset = VOC2007(datadir, split='train', input_transform=input_transform)
         val_dataset = VOC2007(datadir, split='val', input_transform=input_transform)
 
@@ -185,3 +172,19 @@ if __name__ == '__main__':
         # Batches
         for i, (im, label) in enumerate(train_loader):
             print('Batch', i)
+
+        ### Get transformed image, label and transformed bounding box (x,y,w,h) ###
+        print('Loading VOC2007 dataset...')
+        train_dataset = VOC2007(datadir, split='train', input_transform=input_transform)
+        val_dataset = VOC2007(datadir, split='val', input_transform=input_transform)
+
+        train_loader = torch.utils.data.DataLoader(train_dataset,
+                                                   batch_size=args.batch_size,
+                                                   shuffle=True,
+                                                   num_workers=1,
+                                                   pin_memory=True)
+        # Batches
+        for i, (im, label, box) in enumerate(train_loader):
+            print('Batch', i)
+            print('label', label)
+            print('box', box)
