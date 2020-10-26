1a705672f10f2d39b4c8f13ac76cba2f50c6a59b
Bryan Yu <bryany@kth.se>
2020-10-26 15:16:37 +0100

    Experiment 4_1 now uses amrita's trained weights! WOOHOO

--- src/experiments/experiment_4_1/experiment_4_1_compute_heatmaps.py
+++ src/experiments/experiment_4_1/experiment_4_1_compute_heatmaps.py
@@ -5,20 +5,29 @@ from tqdm import tqdm
 from time import sleep
 
 from utils.viz import save_image, get_image, get_normalized_image, normalize, get_heatmap
-from utils.prepare_models import get_model
+from utils.prepare_models import get_model, load_fine_trained
 from model.gradcam import GradCAM
 
+# Path to VOC bounding boxes as parsed from the annotations files
 ground_truth_resized_bounding_boxes_path = './ground_truth_bounding_boxes_original.pickle'
+
+# Path to VOC original images
 VOC2007_images_directory = '../../../datasets/VOC/VOC2007/JPEGImages/'
 
+# Path to fine trained weights
+model_weights = '../../../weights/resnet50_finetrained.ckpt'
 
 if __name__ == "__main__":
+    print("STARTING")
     # please see prepare_models.py for a list of valid models
     # Dated: 21.10.2020; valid_models = ['resnet50','vgg16','googlenet','inception_v3', 'alexnet']
     model_name = 'resnet50'
     
     # Retrieve the model
-    model, layer_name = get_model(model_name)
+    print(f'Loading weights for model: { model_name}...')
+    model, layer_name = load_fine_trained(model_name, model_weights)
+
+    print(f'Weight loaded!')
 
     # Instantiate the gradCAM class
     gradcam = GradCAM(  model=model, \
@@ -29,8 +38,11 @@ if __name__ == "__main__":
     with open(ground_truth_resized_bounding_boxes_path,'rb') as opened_file:
         ground_truth_bounding_boxes = pickle.load(opened_file)
 
+    # Get the list of images to iterate through
+    images = list(ground_truth_bounding_boxes.keys())
+
     # Iterate through every image and compute bounding boxes
-    for image_identifier in tqdm(list(ground_truth_bounding_boxes.keys())):
+    for image_identifier in tqdm(images):
         
         # Strip the .xml from the identifier
         image_identifier = image_identifier.strip('.xml')
--- src/experiments/experiment_4_1/run.sh
+++ src/experiments/experiment_4_1/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# turn on python environment
 adl
-python experiment_4_1_bounding_boxes.py
+
+# Disable if the original bounding boxes have been extracted already
+# python experiment_4_1_bounding_boxes.py
+
+# Extract the heatmaps for each image
 python experiemnt_4_1_compute_heatmaps.py
+
+# Extract the bounding boxes from the heatmaps
 python experiment_4_1.py
+
+# Compute Intersection of union
 python score.py
